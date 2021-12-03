841dfe78349ee63566b34c35547fabf6815093e0
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-12-03 23:49:06 +0100

    write script

--- Backend/lib/server.js
+++ Backend/lib/server.js
@@ -1,3 +1,9 @@
+require("dotenv").config()
 const express = require('express')
 
 const app = express()
+
+const PORT = process.env.PORT || 6600
+app.listen(PORT,()=>{
+    console.log(`port run on port ${PORT}`);
+})
