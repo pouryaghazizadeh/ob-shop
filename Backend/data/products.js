ef82c129915c80a32861ae16173fe299fc300f7a
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-12-04 14:39:39 +0100

    maked data

--- Backend/data/products.js
+++ Backend/data/products.js
@@ -55,44 +55,101 @@ const products = [
   },
   {
     name: "plant",
-    imageUrl:"https://images.unsplash.com/photo-1487700160041-babef9c3cb55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-      
-    description:"this plant is one of the beautiful home plants that you can have and just this need water.",
-     
-    price:23 ,
-    countInStock:14 ,
-  },{
+    imageUrl:
+      "https://images.unsplash.com/photo-1487700160041-babef9c3cb55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "this plant is one of the beautiful home plants that you can have and just this need water.",
+
+    price: 23,
+    countInStock: 14,
+  },
+  {
     name: "bag",
-    imageUrl:"https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-      
-    description:"if you want to go to the mountain you will need this so don't lose it",
-     
-    price:120 ,
+    imageUrl:
+      "https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "if you want to go to the mountain you will need this so don't lose it",
+
+    price: 120,
     countInStock: 23,
-  },{
+  },
+  {
     name: "apple watch",
-    imageUrl:"https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-      
-    description:"a beautiful watch that helps you be healthy and help you watch your motion  in the home and gym",
-     
-    price:450 ,
+    imageUrl:
+      "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "a beautiful watch that helps you be healthy and help you watch your motion  in the home and gym",
+
+    price: 450,
     countInStock: 23,
-  },{
-    name: "",
-    imageUrl:"",
-      
-    description:"",
-     
-    price: ,
-    countInStock: ,
-  },{
-    name: "",
-    imageUrl:"",
-      
-    description:"",
-     
-    price: ,
-    countInStock: ,
+  },
+  {
+    name: "Nikon camera",
+    imageUrl:
+      "https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "take a beautiful photo with a Nikon camera and save your diarise.",
+
+    price: 750,
+    countInStock: 25,
+  },
+  {
+    name: "bicycle",
+    imageUrl:
+      "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description: "be fast and healthy in your life with our bicycle.",
+
+    price: 760,
+    countInStock: 18,
+  },
+  {
+    name: "air pods2",
+    imageUrl:
+      "https://images.unsplash.com/photo-1504274066651-8d31a536b11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "AirPods are wireless Bluetooth earbuds designed by Apple Inc. They were first announced on September 7, 2016 alongside the iPhone 7. Within two years, they became Apple's most popular accessory.",
+
+    price: 399,
+    countInStock: 17,
+  },
+  {
+    name: "watch",
+    imageUrl:
+      "https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "be stylish with our watch. The storied Swiss luxury watch manufacturer International Watch Company, better known as IWC, is one of the titans of the industry. Based in the town of Schaffhausen, this company enjoys widespread fame thanks to their pilot's watches, which they've been producing since the mid-1930s.",
+
+    price: 4300,
+    countInStock: 22,
+  },
+  {
+    name: "giutar",
+    imageUrl:
+      "https://images.unsplash.com/photo-1585930346653-83e98c0ff69c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzV8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "Make sure the playability is comfortable and that the guitar is in excellent condition. Choose between a laminate and solid wood top. If youre on a tight budget, an acoustic guitar with a laminate top might work for. Theyre cheaper than solid wood tops, but they dont vibrate as well.",
+
+    price: 1200,
+    countInStock: 14,
+  },
+  {
+    name: "ps4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1486401899868-0e435ed85128?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI5fHxwcm9kdWN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
+    description:
+      "PlayStation 4 (PS4) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4,  was released on November 12, 202015 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2015 onwards in other major markets except China and India.",
+
+    price: 330,
+    countInStock: 10,
   },
 ];
 
