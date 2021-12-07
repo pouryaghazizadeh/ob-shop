ea6822119dbe00c5afcddee576a0c0f901188b11
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-12-07 20:35:50 +0100

    use redux in home andfix bug

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getAllProducts,getProductsById} = require("../controller/productControllers");
+const {getAllProducts,getProductsById} = require("../controller/productControllers");
   
 //@ desc Get all products from db
 //@ route Get /api/products
--- Frontend/src/page/home/index.js
+++ Frontend/src/page/home/index.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import Product from '../../components/product'
+// tools
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../../components/product/index";
+// actions
+import { getProducts as listProducts } from "../../redux/actions/productActions";
+// style
 import { HomePage } from "./view";
+
 function Home() {
-    return (
-      <HomePage>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </HomePage>
-    );
+  const dispatch = useDispatch();
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+  return <HomePage>
+  
+  
+     {
+        loading ? (<h2>loading...</h2>) : error ? ( <h2>{error}</h2>) : (
+        products.map((product) => {
+        return (
+          <Product
+          key={product._id}
+            imageUrl={product.imageUrl}
+            name={product.name}
+            price={product.price}
+            description={product.description}
+            productId={product._id}
+          />
+        );
+   
+        })
+      )}
+   </HomePage>;
 }
 
-export default Home
+export default Home;
