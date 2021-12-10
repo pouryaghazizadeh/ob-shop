71066c1dd175a38897662102bb94410b237af161
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-12-10 21:56:39 +0100

    style to product component

--- Frontend/src/components/product/view.js
+++ Frontend/src/components/product/view.js
@@ -3,17 +3,20 @@ import styled from "styled-components";
 
 
 
-export const ProductCards =styled.section`
-background: plum;
-width: 300px;
-height: 400px;
-display: flex;
-justify-content: flex-start;
-flex-direction:column;
-align-items: center;
-position: relative;
-
-`
+export const ProductCards = styled.section`
+  background: #fff;
+  width: 300px;
+  height: 400px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  &:hover {
+    box-shadow: 2px 2px 9px rgba(0, 0, 0, 0.3);
+  }
+`;
 
 export const TitleCard = styled.span`
 font-weight: bold;
@@ -25,10 +28,10 @@ font-size: x-large;
 
 
 export const ImgCard = styled.img`
-width: 90%;
-height: 200px;
-
-`
+  width: 90%;
+  height: 200px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`;
 
 export const DescriptionCard = styled.p`
 text-align: center;
@@ -39,17 +42,24 @@ margin: 3px;
 
 
 export const LinkView = styled(Link)`
-width: 98%;
-height: 40px;
-background: red;
-text-align: center;
-text-decoration: none;
-font-size: x-large;
-position: absolute;
-bottom: 8px;
+  width: 98%;
+  height: 40px;
 
+  text-align: center;
+  text-decoration: none;
+  font-size: x-large;
+  position: absolute;
+  bottom: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  color: black;
+  color: ;
+  &:hover {
+    box-shadow: 2px 2px 9px rgba(0, 0, 0, 0.3);
+    background: black;
+    color: #fff;
 
-`
+  }
+`;
 
 export const PriceCard = styled.p`
 font-weight: bold;
