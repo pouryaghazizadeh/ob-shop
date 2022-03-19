4c39e55301cdbdc5f9396c4b8f58f6caf3503028
pourya ghazizadeh <pourya.gh095@gmail.com>
2022-03-19 18:11:23 +0100

    z

---
+++ Backend/routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllProducts,
+  getProductsById,
+} = require("../controller/productControllers");
+
+//@ desc Get all products from db
+//@ route Get /api/products
+//@ access public
+router.get("/", getAllProducts);
+
+//@ desc Get  a products by id from db
+//@ route Get /api/products/:id
+//@ access public
+router.get("/:id", getProductsById);
+
+
+
+module.exports = router;
